e154a2e6b49068854a7dcf3d5da5231472dbdb06
James Rising <jarising@gmail.com>
2024-04-18 14:09:18 -0400

    add new trade template

---
+++ 3-othereffects/trade-io.R
@@ -0,0 +1,8 @@
+load.data <- function(year) {
+    FD.global <<- READ THE FILE
+}
+
+calc.total.loss <- function(directloss) {
+    DO CALCULATIONS ... -> FD after impacts
+    DIVIDE FD after impacts by FD before impact -> loss with trade
+}
