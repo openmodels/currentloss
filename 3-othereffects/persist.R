2b9fdd2c51fd3db08260147cf74be9e0934136af
James Rising <jarising@gmail.com>
2025-01-17 14:07:47 -0500

    add zhao et al

---
+++ 3-othereffects/persist.R
@@ -0,0 +1,10 @@
+## Equation 1:
+## delta log(GDPpc) = \sum_t D_t (1 - \omega)^t
+
+## Equation 2:
+## GDPpc_t = GDPpc_t-1 (1 + g_t - \rho D_t-1)
+## delta log(GDPpc_t) = D_t + \rho D_t-1
+## But then assuming persistence..., so:
+## delta log(GDPpc_t) = \sum_t D_t \rho^t
+
+
