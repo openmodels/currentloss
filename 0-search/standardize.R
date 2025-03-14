c923eb9adf7a717cf67ad180ff9b670f718e42b6
James Rising <jarising@gmail.com>
2025-03-14 16:31:54 -0400

    fix standardization to not drop rows

--- 0-search/standardize.R
+++ 0-search/standardize.R
@@ -39,12 +39,4 @@ for (ii in 1:nrow(papers)) {
     }
 }
 
-df2 <- df.source %>% left_join(df.gemini, by='EID') %>% left_join(df.openai, by='EID', suffix=c('.gemini', '.openai')) %>% filter(!is.na(Verdict.gemini) | !is.na(Verdict.openai))
-df2$XX.common <- paste0(ifelse(df2$XG.gemini & df2$XG.openai, "XG", ""),
-                        ifelse(df2$XE.gemini & df2$XE.openai, "XE", ""),
-                        ifelse(df2$XW.gemini & df2$XW.openai, "XW", ""),
-                        ifelse(df2$XN.gemini & df2$XN.openai, "XN", ""))
-df2$Verdict.common <- ifelse(!is.na(df2$PP.gemini) | !is.na(df2$PP.openai), paste(df2$PP.gemini, df2$PP.openai),
-                      ifelse(df2$XX.common == "", "Disagree", df2$XX.common))
-df2$AI.include <- ifelse(!is.na(df2$PP.gemini) | !is.na(df2$PP.openai), T,
-                  ifelse(df2$XX.common == "", T, F))
+df2 <- df.source %>% left_join(df.gemini, by='EID') %>% left_join(df.openai, by='EID', suffix=c('.gemini', '.openai'))
--- 0-search/stats.R
+++ 0-search/stats.R
@@ -2,5 +2,26 @@ setwd("~/research/currentloss/search2")
 
 library(dplyr)
 
+alldf <- data.frame()
 for (source in c('scopus', 'websci')) {
-    source("standardize.R")
+    source("src/0-search/standardize.R")
+
+    if (source == 'websci') {
+        df3 <- df2[, c('DOI', 'Authors', 'Article Title', 'Source Title', 'Publication Year', 'included', 'XG.gemini', 'XE.gemini', 'XW.gemini', 'XN.gemini', 'PP.gemini', 'XG.openai', 'XE.openai', 'XW.openai', 'XN.openai', 'PP.openai', 'XX.common', 'Verdict.common', 'AI.include')]
+        names(df3)[3:5] <- c('Title', 'Journal', 'Year')
+    } else {
+        df3 <- df2[, c('DOI', 'Authors', 'Title', 'Source.title', 'Year', 'included', 'XG.gemini', 'XE.gemini', 'XW.gemini', 'XN.gemini', 'PP.gemini', 'XG.openai', 'XE.openai', 'XW.openai', 'XN.openai', 'PP.openai', 'XX.common', 'Verdict.common', 'AI.include')]
+        names(df3)[4] <- 'Journal'
+    }
+    df3$source <- source
+
+    df2$XG.common <- ifelse(!is.na(df2$XG.gemini) & !is.na(df2$XG.openai), df2$XG.gemini & df2$XG.openai, NA)
+    df2$XE.common <- ifelse(!is.na(df2$XE.gemini) & !is.na(df2$XE.openai), df2$XE.gemini & df2$XE.openai, NA)
+    df2$XW.common <- ifelse(!is.na(df2$XW.gemini) & !is.na(df2$XW.openai), df2$XW.gemini & df2$XW.openai, NA)
+    df2$XN.common <- ifelse(!is.na(df2$XN.gemini) & !is.na(df2$XN.openai), df2$XN.gemini & df2$XN.openai, NA)
+
+    alldf <- rbind(alldf, df3)
+}
+
+table(alldf$source)
+length(unique(alldf$DOI))
